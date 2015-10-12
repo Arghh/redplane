e7f8fbca18069f63a0bb26b44d89652d884a4cc5
Argo Liivanurm <tammepoeg@gmail.com>
2015-10-12 14:10:48 +0200

    Update Enemy.h

--- Enemy.h
+++ Enemy.h
@@ -2,49 +2,16 @@
 
 class Enemy : public sf::Drawable
 {
-private:
-  sf::Sprite sprite;
-  int speedPerSecond;
-  float distanceTravelled;
-  int maximumDistance;
-
-protected:
-  virtual void draw (sf::RenderTarget &target, sf::RenderStates states) const
-  {
-    target.draw(sprite, states);
-  }
-
 public:
-  Enemy(const sf::Texture& texture, int speedPerSecond, int posx, int posy) : sprite(texture)
-  {
-    this->speedPerSecond = speedPerSecond;
-    this->distanceTravelled = 0;
-    this->maximumDistance = 472;
-    this->sprite.setPosition(posx, posy);
-  }
+    Enemy(const sf::Texture& texture,  float enemyMoveSpeed, float posx, float posy);
+    void updateEnemy(float speedX, float timeDelta);
+    sf::FloatRect getEnemyGlobalBounds() const;
+    bool isEnemyAlive() const;
 
-  void Update(float speedX, float timeDelta)
-  {
-    auto distanceTravlledThisUpdate = speedPerSecond * timeDelta;
-    this->distanceTravelled += distanceTravlledThisUpdate;
-    this->sprite.move(speedX*timeDelta, distanceTravlledThisUpdate);
-    if(this->sprite.getPosition().x > 1600)
-    {
-      this->sprite.setPosition(-800, sprite.getPosition().y);
-    }
-    if(this->sprite.getPosition().x < -800)
-    {
-      this->sprite.setPosition(1600, sprite.getPosition().y);
-    }
-  }
-
-  sf::FloatRect getGlobalBounds() const
-  {
-    return this->sprite.getGlobalBounds();
-  }
-
-  bool IsAlive() const
-  {
-    return this->distanceTravelled < this->maximumDistance;
-  }
+private:
+    sf::Sprite      mSprite;
+    float           mSpeedPerSecond;
+    float           mDistanceTravelled;
+    const float     mMaximumDistance;
+    void draw(sf::RenderTarget &target, sf::RenderStates states) const override;
 };
