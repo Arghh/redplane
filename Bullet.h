b96f1ba1b333d517f90a46e32a2bb41e31113a4a
Argo Liivanurm <tammepoeg@gmail.com>
2015-10-12 14:09:23 +0200

    Update Bullet.h

--- Bullet.h
+++ Bullet.h
@@ -2,53 +2,17 @@
 
 class Bullet : public sf::Drawable
 {
-private:
-  sf::Sprite sprite;
-  bool directionLeft;
-  int speedPerSecond;
-  float distanceTravelled;
-  int maximumDistance;
-
-protected:
-  virtual void draw (sf::RenderTarget &target, sf::RenderStates states) const
-  {
-    target.draw(sprite, states);
-  }
-
 public:
-  Bullet(const sf::Texture& texture, bool directionLeft, int speedPerSecond, int distance, const sf::Vector2f& position) : sprite(texture)
-  {
-    this->directionLeft = directionLeft;
-    this->speedPerSecond = speedPerSecond;
-    this->distanceTravelled = 0;
-    this->maximumDistance = distance;
+    Bullet(const sf::Texture& texture, bool direction, const sf::Vector2f& position);
+    void updateBullet(float timeDelta);
+    sf::FloatRect getBulletGlobalBounds() const;
+    bool isBulletAlive() const;
 
-    this->sprite.setPosition(position);
-  }
-
-  void Update(float timeDelta)
-  {
-    auto distanceTravelledThisUpdate = speedPerSecond * timeDelta;
-
-    this->distanceTravelled += distanceTravelledThisUpdate;
-
-    if(directionLeft) 
-    {
-        this->sprite.move(-distanceTravelledThisUpdate,0);
-    }
-    else
-    {
-    this->sprite.move(distanceTravelledThisUpdate, 0);
-    }
-  }
-
-  sf::FloatRect getGlobalBounds() const
-  {
-    return this->sprite.getGlobalBounds();
-  }
-
-  bool IsAlive() const
-  {
-    return this->distanceTravelled < this->maximumDistance;
-  }
+private:
+    void draw(sf::RenderTarget &target, sf::RenderStates states) const override;
+    sf::Sprite      mSprite;
+    const float     mSpeedPerSecond;
+    float           mDistanceTravelled;
+    const float     mMaximumDistance;
+    bool            direction;
 };
