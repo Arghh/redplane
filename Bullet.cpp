50cd68265f0468469d046e2b6a1b81d9f6f15b7e
Argo Liivanurm <tammepoeg@gmail.com>
2015-10-12 14:09:46 +0200

    Create Bullet.cpp

---
+++ Bullet.cpp
@@ -0,0 +1,42 @@
+#include "Bullet.h"
+
+
+Bullet::Bullet(const sf::Texture& texture, bool directionLeft, const sf::Vector2f& position) :
+  mSprite(texture),
+  mSpeedPerSecond(800.f),
+  mDistanceTravelled(0),
+  mMaximumDistance(1000.f)
+{
+  this->direction = directionLeft;
+  this->mSprite.setPosition(position);
+}
+
+void Bullet::updateBullet(float timeDelta) 
+{
+  auto mDistanceTravelledThisUpdate = mSpeedPerSecond * timeDelta;
+
+  mDistanceTravelled += mDistanceTravelledThisUpdate;
+  if(direction) 
+  {
+    mSprite.move(-mDistanceTravelledThisUpdate, 0);
+  }
+  else
+  {
+    mSprite.move(mDistanceTravelledThisUpdate, 0);
+  }
+}
+
+sf::FloatRect Bullet::getBulletGlobalBounds() const
+{
+  return mSprite.getGlobalBounds();
+}
+
+bool Bullet::isBulletAlive() const
+{
+  return mDistanceTravelled < mMaximumDistance;
+}
+
+void Bullet::draw(sf::RenderTarget &target, sf::RenderStates states) const
+{
+    target.draw(mSprite, states);
+}
